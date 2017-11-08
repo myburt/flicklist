ac1283a3e7671d51baefb49fc9eb2a04b0ea28bc
Mike Burt <myburt@hotmail.com>
2017-11-07 21:32:44 -0600

    Added extra movies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -8,11 +9,18 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
+    movie2 = get_random_movie()
+    movie3 = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
     content += "<li>" + movie + "</li>"
+    content += "</ul>"
+    content += "<h2>Tommorows Movie</h2>"
+    content += "<ul>"
+    content += "<li>" + movie2 + "</li>"
+    content += "<li>" + movie3 + "</li>"
     content += "</ul>"
 
     # TODO: pick another random movie, and display it under
@@ -22,8 +30,10 @@ def index():
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movie_list = ["Dude where's my car", "Ghost Busters", "Austin Powers", "Hell Raiser", "Crouching Tiger Hidden Dragon",
+    "Anchorman", "Shrek", "Lord of the Rings", "Finding Nemo"]
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    return movie_list[random.randrange(0, len(movie_list))]
 
 
 app.run()
