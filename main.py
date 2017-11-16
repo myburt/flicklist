b3f068510ef8b0979e8fb34e9c4db6a1c8020fa1
Mike Burt <myburt@hotmail.com>
2017-11-16 12:59:33 -0600

    Working on getting dropdown list to update

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+movie_list = []
+
 page_header = """
 <!DOCTYPE html>
 <html>
@@ -30,7 +32,11 @@ add_form = """
         <input type="submit" value="Add It"/>
     </form>
 """
-
+def option_adder():
+    temp_html = ''
+    for movie in movie_list:
+        temp_html += '<option value="' + movie + '">' + movie + '</option>'
+    return temp_html
 # TODO:
 # Create the HTML for the form below so the user can check off a movie from their list 
 # when they've watched it.
@@ -38,16 +44,36 @@ add_form = """
 
 # a form for crossing off watched movies
 crossoff_form = """
-
+    <form action="/crossoff" method="post">
+        <label for="remove-movie">
+            Remove: 
+             <select name="remove-movie">
+                {0}
+            </select> 
+            from my watchlist.
+        </label>
+        <input type="submit" value="Remove It"/>
+    </form>
 """
+temp_string = option_adder()
+crossoff_form = crossoff_form.format(temp_string)
 
 # TODO:
 # Finish filling in the function below so that the user will see a message like:
 # "Star Wars has been crossed off your watchlist".
 # And create a route above the function definition to receive and handle the request from 
 # your crossoff_form.
+@app.route("/crossoff", methods=["POST"])
 def crossoff_movie():
-    crossed_off_movie = request.form['crossed-off-movie']    
+    crossed_off_movie = request.form['remove-movie'] 
+    movie_list.remove(crossed_off_movie)
+    # build response content
+    removed_movie_element = "<strong>" + crossed_off_movie + "</strong>"
+    sentence = removed_movie_element + " has been removed from your Watchlist!"
+    content = page_header + "<p>" + sentence + "</p>" + page_footer
+
+    return content
+
 
 # TODO:
 # modify the crossoff_form above to use a dropdown (<select>) instead of
@@ -56,12 +82,12 @@ def crossoff_movie():
 @app.route("/add", methods=['POST'])
 def add_movie():
     new_movie = request.form['new-movie']
-
+    movie_list.append(new_movie)
     # build response content
     new_movie_element = "<strong>" + new_movie + "</strong>"
     sentence = new_movie_element + " has been added to your Watchlist!"
     content = page_header + "<p>" + sentence + "</p>" + page_footer
-
+    content = index() + content
     return content
 
 
@@ -70,7 +96,7 @@ def index():
     edit_header = "<h2>Edit My Watchlist</h2>"
 
     # build the response string
-    content = page_header + edit_header + add_form + page_footer
+    content = page_header + edit_header + add_form + crossoff_form + page_footer
 
     return content
 
